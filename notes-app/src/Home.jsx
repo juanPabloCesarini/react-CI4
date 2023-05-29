6ca409caa34213c054ecb276388e07ac13156b75
juanPabloCesarini <juanpcesarini@hotmail.com>
2023-05-28 21:18:22 -0300

    formulario con uso de onChange para cambiar valor de los estados

--- notes-app/src/Home.jsx
+++ notes-app/src/Home.jsx
@@ -2,6 +2,11 @@ import { useState, React } from 'react'
 
 export default function Dashboard() {
 
+    const initialNote = {
+        id: '',
+        title: '',
+        body: '',
+    }
     const [notes, setNotes] = useState(
         [
             { id: 1, title: 'nota1', body: 'lorem ipsum' },
@@ -12,12 +17,29 @@ export default function Dashboard() {
         ]
     );
 
-    const changeState = ()=>{
-        const reg = { id: 6, title: 'nota6', body: 'lorem ipsum' };
-
-        setNotes(notes.concat(reg));
-    }
+    /*     const changeState = ()=>{
+            const reg = { id: 6, title: 'nota6', body: 'lorem ipsum' };
     
+            setNotes(notes.concat(reg));
+        }
+         */
+    const [note, setNote] = useState(initialNote);
+
+    const addNote = (ev) => {
+        ev.preventDefault();
+
+        if(note.title==="" || note.body ===""){
+            return
+        }
+        setNotes([
+            ...notes,
+            {
+                ...note,
+                id: Math.max(...notes.map(note => note.id)) + 1
+            }
+        ]); 
+       // console.log(Math.max(...notes.map(note => note.id)) + 1)
+    }
     return (
         <>
 
@@ -26,13 +48,27 @@ export default function Dashboard() {
                 {
                     // la función map() sirve para iterar objetos
                     notes.map(note => {
-                        return <li>Id: {note.id} Titulo: {note.title} Body: {note.body}</li>;
+                        return <li key={note.id}>Id: {note.id} Titulo: {note.title} Body: {note.body}</li>;
                     })
                 }
             </ul>
-            <button onClick={()=>changeState()}>Cambiar Estado</button>
+            <form onSubmit={(ev) => addNote(ev)}>
+                <label htmlFor="title">
+                    Titulo
+                </label>
+                <input type="text" name="" id="title" onChange={(e) => setNote({ ...note, title: e.target.value })} />
+                <br />
+                <label htmlFor="body">
+                    Cuerpo
+                </label>
+                <input type="text" name="" id="body" onChange={(e) => setNote({ ...note, body: e.target.value })} />
+                <br />
+                <button>Agregar</button>
+            </form>
+            {/* <button onClick={()=>changeState()}>Cambiar Estado</button> */}
+            
         </>
-        
+
 
     )
 }
