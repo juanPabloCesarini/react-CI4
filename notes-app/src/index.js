e28ea3bf33342dc7d4bb07f2104e94b41e6a7055
juanPabloCesarini <juanpcesarini@hotmail.com>
2024-10-21 11:31:34 -0300

    los primeros 5 videos

---
+++ notes-app/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+//import App from './App';
+import Dashboard from './Home';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+   <Dashboard></Dashboard>
+  </React.StrictMode>
+);
+
+
